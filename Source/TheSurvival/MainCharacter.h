7a02171863f10c37e24119985ab42f3e742f25ce
HorizonBlade <kabanhokage309@gmail.com>
2025-02-17 17:56:27 +0100

    added camera component for main character

--- Source/TheSurvival/MainCharacter.h
+++ Source/TheSurvival/MainCharacter.h
@@ -1,9 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 #include "MainCharacter.generated.h"
 
 UCLASS()
@@ -12,18 +14,19 @@ class THESURVIVAL_API AMainCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AMainCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	USpringArmComponent* CameraBoom;
 
-	// Called to bind functionality to input
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* CameraComponent;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
