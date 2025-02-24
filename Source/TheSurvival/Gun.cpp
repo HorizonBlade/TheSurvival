d0c5fbb4c221f9c3ee998093e6b994b80458630d
HorizonBlade <kabanhokage309@gmail.com>
2025-02-24 18:19:22 +0100

    redid the Fire trace

--- Source/TheSurvival/Gun.cpp
+++ Source/TheSurvival/Gun.cpp
@@ -46,13 +46,33 @@ void AGun::Fire()
 
     UE_LOG(LogTemp, Warning, TEXT("Fire! Weapon: %s, Ammo in clip: %d"), *WeaponType.ToString(), CurrentAmmoInClip);
 
-    FHitResult HitResult;
-    FVector Start = GetActorLocation();
-    FVector ForwardVector = GetActorForwardVector();
+    FVector EyeLocation;
+    FRotator EyeRotation;
+    OwnerCharacter->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+    FVector Start = EyeLocation;
+    FVector ForwardVector = EyeRotation.Vector();
     FVector End = Start + (ForwardVector * 5000);
 
+    FHitResult HitResult;
     FCollisionQueryParams CollisionParams;
     CollisionParams.AddIgnoredActor(this);
+
+    if (GEngine)
+    {
+        if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, CollisionParams))
+        {
+            DrawDebugLine(GetWorld(), Start, HitResult.Location, FColor::Red, false, 1.0f, 0, 1.0f);
+            DrawDebugSphere(GetWorld(), HitResult.Location, 10.0f, 12, FColor::Blue, false, 1.0f, 0, 1.0f);
+
+            DealDamage(HitResult.GetActor());
+        }
+        else
+        {
+            DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 1.0f, 0, 1.0f);
+            DrawDebugSphere(GetWorld(), End, 10.0f, 12, FColor::Blue, false, 1.0f, 0, 1.0f);
+        }
+    }
 
     if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, CollisionParams))
     {
